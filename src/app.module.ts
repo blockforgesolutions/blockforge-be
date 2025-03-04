7d68dfbbf649688a7e6c5ed1a3905b814ca3efa6
Yesarib <yesaribaris23@gmail.com>
2025-03-04 13:57:53 +0300

    feat: add mongoose and global config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CourseModule } from './course/course.module';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL!, {
+      dbName: 'dashboard'
+    }),
+    CourseModule,
+    LessonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
