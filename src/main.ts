6dc3e980f20f6949f7351038405513468b33cd29
Yesarib <yesaribaris23@gmail.com>
2025-03-19 16:12:08 +0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,17 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new AllExceptionsFilter())
+
+  const config = new DocumentBuilder()
+    .setTitle('Cohort Platform API')
+    .setDescription('The cohort platform API description')
+    .setVersion('1.0') 
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${process.env.PORT}`);
   
