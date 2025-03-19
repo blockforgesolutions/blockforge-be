796367fe9d56f66c3811373c59c1cf5d9d379a0a
Yesarib <yesaribaris23@gmail.com>
2025-03-19 15:01:33 +0300

    feat: add role on course and lesson apis

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { ApiResponseDto } from 'src/common/dto/response.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('course')
 export class CourseController {
@@ -11,6 +14,8 @@ export class CourseController {
     ) {}
 
     @Post()
+    @UseGuards(JwtAuthGuard,RoleGuard)
+    @Roles('INSTRUCTOR',"ADMIN")
     async createCourse(@Body() course: CreateCourseDto) {
         const newCourse = await this.courseService.createCourse(course);
         return new ApiResponseDto(true, newCourse);
@@ -29,12 +34,16 @@ export class CourseController {
     }
 
     @Put(':courseId')
+    @UseGuards(JwtAuthGuard,RoleGuard)
+    @Roles('INSTRUCTOR',"ADMIN")
     async updateCourse(@Param('courseId') courseId: string, @Body() course: UpdateCourseDto) {
         const updatedCourse = await this.courseService.updateCourse(courseId, course);
         return new ApiResponseDto(true, updatedCourse);
     }
 
     @Delete(':courseId')
+    @UseGuards(JwtAuthGuard,RoleGuard)
+    @Roles("ADMIN")
     async deleteCourse(@Param('courseId') courseId: string) {
         const deletedCourse = await this.courseService.deleteCourse(courseId);
         return new ApiResponseDto(true, deletedCourse);
--- src/lesson/lesson.controller.ts
+++ src/lesson/lesson.controller.ts
@@ -1,50 +1,59 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { LessonService } from './lesson.service';
 import { CreateLessonDto } from './dto/create-lesson.dto';
 import { ApiResponseDto } from 'src/common/dto/response.dto';
 import { UpdateLessonDto } from './dto/update-lesson.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RoleGuard } from 'src/common/guards/role.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 
 @Controller('lesson')
 export class LessonController {
     constructor(
         private readonly lessonService: LessonService
-    ) {}
+    ) { }
 
     @Post()
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles('INSTRUCTOR', "ADMIN")
     async createLesson(@Body() lesson: CreateLessonDto) {
         const newLesson = await this.lessonService.createLesson(lesson);
 
-        return new ApiResponseDto(true,newLesson);
+        return new ApiResponseDto(true, newLesson);
     }
 
     @Get()
     async getLessons() {
         const lessons = await this.lessonService.getLessons();
 
-        return new ApiResponseDto(true,lessons);
+        return new ApiResponseDto(true, lessons);
     }
 
     @Get(':lessonId')
     async getLessonById(@Param('lessonId') lessonId: string) {
         const lesson = await this.lessonService.getLessonById(lessonId);
-        return new ApiResponseDto(true,lesson);
+        return new ApiResponseDto(true, lesson);
     }
 
     @Get('getCourseLessons/:courseId')
     async getLessonsByCourseId(@Param('courseId') courseId: string) {
         const lessons = await this.lessonService.getLessonsByCourseId(courseId);
-        return new ApiResponseDto(true,lessons);
+        return new ApiResponseDto(true, lessons);
     }
 
     @Put(':lessonId')
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles('INSTRUCTOR', "ADMIN")
     async updateLesson(@Param('lessonId') lessonId: string, @Body() lesson: UpdateLessonDto) {
         const updatedLesson = await this.lessonService.updateLesson(lessonId, lesson);
-        return new ApiResponseDto(true,updatedLesson);
+        return new ApiResponseDto(true, updatedLesson);
     }
 
     @Delete(':lessonId')
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles('INSTRUCTOR', "ADMIN")
     async deleteLesson(@Param('lessonId') lessonId: string) {
         const deletedLesson = await this.lessonService.deleteLesson(lessonId);
-        return new ApiResponseDto(true,deletedLesson);
+        return new ApiResponseDto(true, deletedLesson);
     }
 }
