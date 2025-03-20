2a3b6a59c6f1dedc736add2324c7ec6965aa5a8c
Yesarib <yesaribaris23@gmail.com>
2025-03-20 14:59:21 +0300

    feat: add transformMongoData utility function for transforming MongoDB documents to DTOs

---
+++ src/common/utils/transform.utils.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { transformMongoDocument } from './mongo.utils';
+
+interface MongoDocument<T> {
+    _id: any;
+    __v?: any;
+}
+
+import { plainToInstance } from 'class-transformer';
+
+export function transformMongoData<T extends MongoDocument<T>, R>(
+    data: T,
+    dtoClass: new () => R
+): R {
+    const transformedData = transformMongoDocument(data);
+    if (!transformedData) {
+        throw new HttpException(
+            { message: 'Data not found', statusCode: HttpStatus.NOT_FOUND },
+            HttpStatus.NOT_FOUND,
+        );
+    }
+    return plainToInstance(dtoClass, transformedData);
+}
